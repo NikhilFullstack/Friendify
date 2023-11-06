98d37ab8002ddece79b28e6b5dab9193208aef21
Nikhil Gupta <Gup7nik@gmail.com>
2023-11-06 23:25:27 +0530

    sda

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -120,8 +120,7 @@ export function authz(token, navigate){
           console.log("Authz API RESPONSE............", response)
           if (!response?.data?.success) {
             toast.dismiss(toastId)
-            await dispatch(logout)   
-            navigate("/login")
+            await dispatch(logout(navigate))   
 
             throw new Error("Login Time Up Signin Again")
           }
@@ -129,9 +128,7 @@ export function authz(token, navigate){
           toast.dismiss(toastId);
           console.log("Authz user Api error............", error)
           toast.error(error.message)
-          await dispatch(logout)
-          navigate("/login")
-
+          await logout(navigate);
         }
         dispatch(setLoading(false))
         toast.dismiss(toastId)
